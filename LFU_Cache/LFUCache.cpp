94cff24d018b6f125eb5ef05974eae1442c5a8d5
Aakash Kumar <your-email@gmail.com>
2025-12-19 10:57:23 +0530

    Implement LFUCache constructor and get operation

--- LFU_Cache/LFUCache.cpp
+++ LFU_Cache/LFUCache.cpp
@@ -1,1 +1,22 @@
-File craeted
+#include "LFUCache.h"
+
+LFUCache::LFUCache(int capacity){
+    this->capacity = capacity;
+    this->minFreq = 0 ;
+}
+int LFUCache::get(int key){
+    if(keyNode.find(key) == keyNode.end()){
+        return -1;
+    }
+    Node* node = keyNode[key];
+    int currFreq = node->freq;
+
+    freqList[currFreq].removeNode(node);
+
+    if(currFreq == minFreq && freqList[currFreq].size() ==0){
+        minFreq++;
+    }
+    node->freq++;
+    freqList[node->freq].addFront(node);
+    return node->value;
+}
