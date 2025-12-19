fa797fa85851bcb368070968b81d979de3940af9
Aakash Kumar <your-email@gmail.com>
2025-12-19 08:34:25 +0530

    Fix Node constructor initialization for LFU cache

--- Data_Structures/LinkedList/DoublyLinkedList.h
+++ Data_Structures/LinkedList/DoublyLinkedList.h
@@ -1,1 +1,29 @@
+class Node{
+public:    
+    int key;
+    int value;
+    int freq;
+    Node* next;
+    Node* prev;
+
+    Node(int key,int value){
+        this->key = key;
+        this->value =value;
+        this->freq = 1;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
+class DoublyLinkedList{
+public:
+    Node* head;
+    Node* tail;
+    int size;
+    
+    DoublyLinkedList(){
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    }
+};
 
