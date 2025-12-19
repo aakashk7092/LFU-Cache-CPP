5e55c2312039fc49c53adaacf9dda3850bcb85a8
Aakash Kumar <your-email@gmail.com>
2025-12-19 08:47:02 +0530

    LFU Cache structure

--- LFU_Cache/LFUCache.h
+++ LFU_Cache/LFUCache.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <unordered_map>
+#include "../Data_Structures/LinkedList/DoublyLinkedList.h"
 
+class LFUCache{
+private:    
+    int capacity;
+    int minFreq;
+
+    std::unordered_map<int,Node*> keyNode;
+    std::unordered_map<int,DoublyLinkedList> freqList;
+public:
+    LFUCache(int capacity);
+    int get(int key);
+    void put(int key ,int value);
+};
