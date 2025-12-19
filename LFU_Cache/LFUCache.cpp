87dcf614d2d7b0415df56264d3fb56fa64beb6bb
Aakash Kumar <your-email@gmail.com>
2025-12-19 11:33:43 +0530

    Add main.cpp to test LFU cache functionality

--- LFU_Cache/LFUCache.cpp
+++ LFU_Cache/LFUCache.cpp
@@ -2,21 +2,52 @@
 
 LFUCache::LFUCache(int capacity){
     this->capacity = capacity;
-    this->minFreq = 0 ;
+    this->minFreq = 0;
 }
+
 int LFUCache::get(int key){
     if(keyNode.find(key) == keyNode.end()){
         return -1;
     }
+
     Node* node = keyNode[key];
     int currFreq = node->freq;
 
     freqList[currFreq].removeNode(node);
 
-    if(currFreq == minFreq && freqList[currFreq].size() ==0){
+    if(currFreq == minFreq && freqList[currFreq].size == 0){
         minFreq++;
     }
+
     node->freq++;
     freqList[node->freq].addFront(node);
+
     return node->value;
+}
+
+void LFUCache::put(int key ,int value){
+    if(capacity == 0) return;
+
+    if(keyNode.find(key) != keyNode.end()){
+        Node* node = keyNode[key];
+        node->value = value;
+        get(key); 
+        return;
+    }
+
+    if (keyNode.size() == capacity) {
+        DoublyLinkedList &list = freqList[minFreq];
+        Node* nodeToRemove = list.removeLast();
+
+        if (nodeToRemove != nullptr) {
+            keyNode.erase(nodeToRemove->key);
+            delete nodeToRemove;
+        }
+    }
+
+    Node* newNode = new Node(key, value);
+    minFreq = 1;
+
+    freqList[1].addFront(newNode);
+    keyNode[key] = newNode;
 }
--- LFU_Cache/main.cpp
+++ LFU_Cache/main.cpp
@@ -1,1 +1,25 @@
-main file created
+#include <iostream>
+#include "LFUCache.h"
+
+int main() {
+
+    LFUCache cache(2);
+
+    cache.put(1, 10);   
+    cache.put(2, 20);   
+
+    std::cout << cache.get(1) << std::endl;
+    cache.put(3, 30);
+
+    std::cout << cache.get(2) << std::endl;
+    std::cout << cache.get(3) << std::endl;
+    std::cout << cache.get(1) << std::endl; 
+
+    cache.put(4, 40);
+
+    std::cout << cache.get(3) << std::endl; 
+    std::cout << cache.get(4) << std::endl; 
+    std::cout << cache.get(1) << std::endl; 
+
+    return 0;
+}
